db5d12b1cb277d85296676989cc003d083590974
Md Khairul Islam <khairul@orangetoolz.com>
2019-12-05 17:41:55 +0600

    Associative array with loop

--- index.php
+++ index.php
@@ -20,5 +20,32 @@ echo "<br>";
 var_dump(returnType(5,2));
 
 
+$student = [
+  '12' => 'Hasan',
+  '13' => 'Korim',
+  '14' => 'Jalal',
+];
+
+//var_dump($student[12]);
+
+$foods = [
+    'vegetables' => 'capsicam, carrot, brinjal',
+    'fruits' => 'orange, banana, apple',
+];
+
+//var_dump($foods['vegetables']);
+
+foreach ($foods as $key=>$value){
+    echo $key . '=' . $value . '<br>';
+}
+
+
+
+
+
+
+
+
+
 
 
